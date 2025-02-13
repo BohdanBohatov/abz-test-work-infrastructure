399e699b3b6961e1494f543c59aa72797db6b197
Bohdan <bbohatovmif@gmail.com>
2025-02-13 11:44:23 +0200

    Added creating key pair and ec2 to infrastructure creating script

--- infrastructure_destroying.sh
+++ infrastructure_destroying.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+terraform -chdir=dev/ec2 destroy -auto-approve
+terraform -chdir=dev/key_pair destroy -auto-approve
 terraform -chdir=dev/ec2_security_groups destroy -auto-approve
 terraform -chdir=dev/subnet destroy -auto-approve
 terraform -chdir=dev/vpc destroy -auto-approve
--- infrastructure_initialization.sh
+++ infrastructure_initialization.sh
@@ -8,3 +8,9 @@ terraform -chdir=dev/subnet apply -auto-approve
 
 terraform -chdir=dev/ec2_security_groups init
 terraform -chdir=dev/ec2_security_groups apply -auto-approve
+
+terraform -chdir=dev/key_pair init
+terraform -chdir=dev/key_pair apply -auto-approve
+
+terraform -chdir=dev/ec2 init
+terraform -chdir=dev/ec2 apply -auto-approve
