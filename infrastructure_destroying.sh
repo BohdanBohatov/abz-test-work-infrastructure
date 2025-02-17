81d2462dff9af6c1bff418004bca40e8a8846d5f
Bohdan <bbohatovmif@gmail.com>
2025-02-17 16:53:34 +0200

    Finished script for creating and destroying infrastructure

--- infrastructure_destroying.sh
+++ infrastructure_destroying.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+terraform -chdir=dev/databases/elasticache_redis destroy -auto-approve
+terraform -chdir=dev/databases/mysql_rds destroy -auto-approve
 terraform -chdir=dev/ec2 destroy -auto-approve
 terraform -chdir=dev/key_pair destroy -auto-approve
-terraform -chdir=dev/ec2_security_groups destroy -auto-approve
+
+terraform -chdir=dev/security_groups/ec2_security_groups destroy -auto-approve
+terraform -chdir=dev/security_groups/mysql_security_group destroy -auto-approve
+terraform -chdir=dev/security_groups/elasticache_redis_security_group destroy -auto-approve
+
 terraform -chdir=dev/subnet destroy -auto-approve
 terraform -chdir=dev/vpc destroy -auto-approve
--- infrastructure_initialization.sh
+++ infrastructure_initialization.sh
@@ -6,11 +6,24 @@ terraform -chdir=dev/vpc apply -auto-approve
 terraform -chdir=dev/subnet init
 terraform -chdir=dev/subnet apply -auto-approve
 
-terraform -chdir=dev/ec2_security_groups init
-terraform -chdir=dev/ec2_security_groups apply -auto-approve
+terraform -chdir=dev/security_groups/ec2_security_groups init
+terraform -chdir=dev/security_groups/ec2_security_groups apply -auto-approve
 
 terraform -chdir=dev/key_pair init
 terraform -chdir=dev/key_pair apply -auto-approve
 
 terraform -chdir=dev/ec2 init
 terraform -chdir=dev/ec2 apply -auto-approve
+
+terraform -chdir=dev/security_groups/mysql_security_group init
+terraform -chdir=dev/security_groups/mysql_security_group apply -auto-approve
+
+terraform -chdir=dev/databases/mysql_rds init
+terraform -chdir=dev/databases/mysql_rds apply -auto-approve
+
+#Install elasticache redis
+terraform -chdir=dev/security_groups/elasticache_redis_security_group init
+terraform -chdir=dev/security_groups/elasticache_redis_security_group apply -auto-approve
+
+terraform -chdir=dev/databases/elasticache_redis init
+terraform -chdir=dev/databases/elasticache_redis apply -auto-approve
